40c88406501eaabc426cf8a974048f37c5de1225
Sydney Barlow <sydneybarlow44@gmail.com>
2018-05-26 03:36:40 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,11 @@
 def speak_to_grandma(phrase)
-if phrase == "I LOVE YOU GRANDMA!"
+  if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
   else phrase == phrase.downcase
     return "HUH?! SPEAK UP, SONNY!"
-end  
+  end  
 end
 
 # Write a speak_to_grandma method.
